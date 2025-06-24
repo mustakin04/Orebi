a32a300d7782dc209faaf347d841d12cf970880b
mustakin04 <mustakinhasan37@gmail.com>
2025-06-24 14:01:10 +0600

    Advertise part compeleted

---
+++ OribeEcommerce/src/Component/Advertise/Advertise.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Container from "../Container/Container";
+import ad1 from "../../assets/Ad_1.png";
+import ad2 from "../../assets/Ad_2.png";
+import ad3 from "../../assets/Ad_3.png";
+
+const Advertise = () => {
+  return (
+    <div>
+      <Container>
+        <div className="flex justify-between">
+          <div>
+            <img src={ad1} alt="" />
+          </div>
+          <div className="flex justify-between">
+            <div>
+              <div className="">
+                <img src={ad2} alt="" />
+              </div>
+              <div>
+                <img src={ad3} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Advertise;
--- OribeEcommerce/src/Pages/Home.jsx
+++ OribeEcommerce/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../Component/Banner/Banner'
 import AdditonalInfo from '../Component/AdditonalInfo/AdditonalInfo'
+import Advertise from '../Component/Advertise/Advertise'
 
 const Home = () => {
   return (
     <div>
         <Banner></Banner>
         <AdditonalInfo></AdditonalInfo>
+        <Advertise></Advertise>
     </div>
   )
 }
